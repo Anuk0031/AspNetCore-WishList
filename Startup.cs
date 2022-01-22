4be2a720d939fca31337ab9561dafffbb911d9a4
Anuk0031 <anushreek0031@gmail.com>
2022-01-22 10:59:19 +0530

    commit changes in startup

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,10 +24,11 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
-          {
-            
+            app.Run(async (context) =>
             if (env.IsDevelopment())
-           {
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -37,53 +37,7 @@ namespace WishList
             }
 
             app.UseRouting();
-            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
-        }        
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });       
 }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-namespace WishList
-{
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-		services.AddMVC();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-
-          {
-            
-            if (env.IsDevelopment())
-           {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
-        }        
-}
-    }
-}
->>>>>>> 54da719de85136c6ab35cd20250484f1ec095ddf
